49a0f89941ac1a4257ede4b85fe40f47e8ebd6ea
joshitrejimon1 <joshit.rejimon@aurionpro.com>
2023-09-23 14:58:23 +0530

    Add project files.

---
+++ Model/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp_EVENTS_.Model
+{
+    delegate void DBalanceCHange(Account account);
+    internal class Account// PUBLISHER 
+    {
+        public int AccountNo { get; set; }
+        public string AccountName { get; set; }
+        public double Balance { get; set; }
+
+        public event DBalanceCHange OnBalanceChanged;// default null 
+
+
+        public void Desposite(double amount)
+        {
+            Balance += amount;
+            if (OnBalanceChanged != null)// check for aubscribers 
+                OnBalanceChanged(this);
+
+        }
+
+        public Account(int accountNo, string name, double balance)
+        {
+            AccountNo = accountNo;
+            AccountName = name;
+            Balance = balance;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using AccountApp_EVENTS_.Model;
+
+namespace AccountApp_EVENTS_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(101, "Joshit", 5000);
+
+            account1.OnBalanceChanged += SendSMS;
+            account1.OnBalanceChanged += SendEmail;
+
+            account1.Desposite(1000);
+        }
+
+        static void SendSMS(Account account)
+        {
+            Console.WriteLine("sending sms " +
+                "Main Balance is " + account.Balance);
+        }
+        static void SendEmail(Account account)
+        {
+            Console.WriteLine("sending Email " +
+                "Main Balance is " + account.Balance);
+        }
+    }
+}
